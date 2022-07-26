c9c19736f0705057e1efda0a4c37c42a3fe434b8
mesutkacmaz <mesut.kacmaz@outlook.com>
2022-07-26 22:37:47 +0300

    Fixed database security issue

--- v1/src/loaders/db.js
+++ v1/src/loaders/db.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(
-    `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-    {
+  let conn
+  if (process.env.NODE_ENV === 'development') {
+    conn = await mongoose.connect(
+      `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+      {
+        connectTimeoutMS: 1000,
+      }
+    )
+  } else if (process.env.NODE_ENV === 'production') {
+    conn = await mongoose.connect(process.env.MONGO_URI, {
       connectTimeoutMS: 1000,
-    }
-  )
+    })
+  }
 
   console.log(`MongoDB Connected: ${conn.connection.host}`)
 }
